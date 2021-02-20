d146c7a23349d5de769327215f4cd3d477c09679
Bar <Barg314@gmail.com>
2021-02-20 16:24:08 +0200

    working server with uniqe_ptr

--- Chat/Server/Server.cpp
+++ Chat/Server/Server.cpp
@@ -6,30 +6,22 @@ Server::Server(boost::asio::io_service& io_service, uint32_t port) :
 	startAccept();
 }
 
-Server::~Server()
-{
-	for each (User* user in _users)
-	{
-		delete user;
-	}
-}
-
 void Server::startAccept()
 {
-	User* newUser = new User(_io_service);
-	_acceptor.async_accept(newUser->getSocket(), boost::bind(&Server::handleAccept, this, std::move(newUser), boost::asio::placeholders::error));
+	_newUser = std::make_unique<User>(_io_service);
+	_acceptor.async_accept(_newUser->getSocket(), boost::bind(&Server::handleAccept, this, boost::asio::placeholders::error));
 }
 
-void Server::handleAccept(User* newUser, boost::system::error_code ec)
+void Server::handleAccept(boost::system::error_code ec)
 {
 	if (!ec)
 	{
 		boost::asio::streambuf buffer;
-		boost::asio::read_until(newUser->getSocket(), buffer, '\0');
+		boost::asio::read_until(_newUser->getSocket(), buffer, '\0');
 		std::string name((std::istreambuf_iterator<char>(&buffer)), std::istreambuf_iterator<char>());
-		newUser->setName(name);
-		_users.emplace_back(std::move(newUser));
-		std::cout << "New User: " << newUser->getName() << std::endl;
+		_newUser->setName(name);
+		std::cout << "New User: " << _newUser->getName() << std::endl;
+		_users.emplace_back(std::move(_newUser));
 	}
 	else
 	{
--- Chat/Server/Server.hpp
+++ Chat/Server/Server.hpp
@@ -10,17 +10,15 @@ class Server
 {
 public:
 	Server(boost::asio::io_service& io_service, uint32_t port);
-	~Server();
 
 private:
 	void startAccept();
-	void handleAccept(User* newUser, boost::system::error_code ec);
-
-	boost::asio::io_service& _io_service;
-	boost::asio::ip::tcp::acceptor _acceptor;
+	void handleAccept(boost::system::error_code ec);
 
 	uint32_t _port;
-
-	std::vector<User*> _users;
+	boost::asio::io_service& _io_service;
+	boost::asio::ip::tcp::acceptor _acceptor;
+	std::unique_ptr<User> _newUser;	
+	std::vector<std::unique_ptr<User>> _users;
 };
 
